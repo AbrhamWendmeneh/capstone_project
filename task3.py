99448979ce7e9d325877cdc0d11dc1199277b36c
Abrham Wendmeneh <99592824+AbrhamWendmeneh@users.noreply.github.com>
2023-12-07 13:35:10 +0300

    I have finished simple registration form

--- task3.py
+++ task3.py
@@ -19,8 +19,8 @@ from typing import Dict,Any
 class Form(StatesGroup):
     fullName= State()
     gender= State()
-    age=State()
-    email=State()
+    age= State()
+    email= State()
     
 load_dotenv('.env')
 token=os.getenv('TOKEN_API_2')
@@ -28,33 +28,34 @@ bot= Bot(token)
 dp=Dispatcher()
 form_router = Router()
     
+
 @form_router.message(CommandStart())
-async def cmd_start(msg: types.Message, state_val: FSMContext):
-    await state_val.set_state(Form.fullName)
+async def cmd_start(msg: types.Message, state: FSMContext):
+    await state.set_state(Form.fullName)
     await msg.answer(
                     "hi, This is simple form for regitration, and write your name.",
-                     reply_markup=ReplyKeyboardRemove()
+                     reply_markup=ReplyKeyboardRemove(),
                     )
     
     
 @form_router.message(Command("Cancel"))
 @form_router.message(F.text.casefold()=="cancel")
-async def cancel_handler(msg: types.Message, state_val: FSMContext):
-    current_state= await state_val.get_state()
-    if current_state is None:
+async def cmd_cancel(msg: types.Message, state: FSMContext):
+    curr_state_val= await state.get_state()
+    if curr_state_val is None:
         return
-    logging.info("Cancelling state %r", current_state)
-    await state_val.clear()
+    logging.info("Cancelling state %r", curr_state_val)
+    await state.clear()
     await msg.answer(
-        "cacelled",
-        reply_markup= ReplyKeyboardRemove()
+        "cancelled",
+        reply_markup= ReplyKeyboardRemove(),
     )
     
 # This is to take values
 @form_router.message(Form.fullName)
-async def take_name(msg: types.Message, state_val:FSMContext):
-    await state_val.update_data(fullName= msg.text)
-    await state_val.set_state(Form.gender)
+async def take_name(msg: types.Message, state: FSMContext):
+    await state.update_data(fullName= msg.text)
+    await state.set_state(Form.gender)
     await msg.answer(
                     f"hey,could you please choose your gender",
                     reply_markup=ReplyKeyboardMarkup(
@@ -70,27 +71,33 @@ async def take_name(msg: types.Message, state_val:FSMContext):
 
 #  This is for the case of the gender
 @form_router.message(Form.gender)
-async def take_gender(msg: types.Message, state_val:FSMContext):
-    await state_val.update_data(gender= msg.text)
-    await state_val.set_data(Form.age)
+async def take_gender(msg: types.Message, state:FSMContext):
+    if msg.text.casefold()=="cancel":
+        await cmd_cancel(msg, state)
+        return
+    await state.update_data(gender= msg.text)
+    await state.set_state(Form.age)
     await msg.answer(
         "could you please enter your age")
+    
+
     
     
 
     
 @form_router.message(Form.age)
-async def take_age(msg: types.Message, state_val:FSMContext):
-    await state_val.update_data(age=msg.text)
-    await state_val.set_data(Form.email)
+async def take_age(msg: types.Message, state:FSMContext):
+    await state.update_data(age=msg.text)
+    await state.set_state(Form.email)
     await msg.answer(
         "could you please enter your email")
     
     
 @form_router.message(Form.email)
-async def take_email(msg: types.Message, state_val:FSMContext):
-    result=await state_val.update_data(email= msg.text)
+async def take_email(msg: types.Message, state:FSMContext):
+    result=await state.update_data(email= msg.text)
     await form_summary(msg= msg, result=result)
+    await state.finish()
     
 async def form_summary(msg: types.Message, result: Dict[str,Any]):
     fullName= result['fullName']
@@ -98,13 +105,13 @@ async def form_summary(msg: types.Message, result: Dict[str,Any]):
     age= result['age']
     email= result['email']
     
-    summray= "Registration summary"
-    summray+=f"your name {fullName.text}"
-    summray+= f"your gender{ gender.text}"
-    summray+=f"your age{ age.text}"
-    summray+=f"your email address{email.text}"
+    summray= "Registration summary\n"
+    summray+=f"Name: {fullName}\n"
+    summray+= f"Gender: { gender}\n"
+    summray+=f"Age: {age}\n"
+    summray+=f"Email: {email}\n"
     
-    await msg.answer(summray, reply_markup= ReplyKeyboardRemove)
+    await msg.answer(summray, reply_markup= ReplyKeyboardRemove())
     
    
     
